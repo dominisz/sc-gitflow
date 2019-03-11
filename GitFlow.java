e91a2e09cb078cf78db82c671a7f7aaa6c8d0849
vladyslav.tehlivets <vladyslav.tehlivets@infinite.pl>
2019-03-11 18:33:04 +0100

    feature/3 add supplier implementation

--- GitFlow.java
+++ GitFlow.java
@@ -2,7 +2,7 @@ import java.util.function.Supplier;
 
 public class GitFlow {
 
-    private static final Supplier[] SUPPLIERS = {new G21()};
+    private static final Supplier[] SUPPLIERS = {new G21(), new G23()};
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
